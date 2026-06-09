ef03acedd6fab00e89263505c2b4dd8e1bd14151
Adeyemo Ridwan <adeyemoridwan97@gmail.com>
2026-06-09 20:56:11 +0100

    fix: restore AuthContext, remove AuthProvider from main.jsx, fix build

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './index.css'
 import './dark-mode.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-import { AuthProvider } from './context/AuthContext'
+
 import { PiProvider } from './context/PiContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <PiProvider>
-        <AuthProvider>
+        
         <App />
-        </AuthProvider>
+        
       </PiProvider>
     </ThemeProvider>
   </StrictMode>,
